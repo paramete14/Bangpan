19ea5c91d0d44e636cb502755d2d307a103abba0
lada60010876 <60010876@kmitl.ac.th>
2019-10-23 21:13:19 +0700

    3 DB connect

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 const express=require('express');
-const app =express();
+const mongoose = require('mongoose')
 require("dotenv").config();
 
+//app
+const app =express();
+
+//database
+mongoose.connect(process.env.DATABASE, { 
+    useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true 
+}).then(()=>console.log("DB connected"));
+
+//routes
 app.get("/",(req,res)=>{
     res.send("hello from node")
 });
